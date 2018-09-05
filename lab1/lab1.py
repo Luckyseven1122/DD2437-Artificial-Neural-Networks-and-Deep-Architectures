35356f6dc27dd77a4c7e65b2edd7f79cc4c44392
victorhv <victorhv@kth.se>
2018-09-05 10:37:37 +0200

    fixfix

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -11,14 +11,14 @@ def generate_binary_data():
     '''
     n_points = 100
     mA = np.array([ 1.3, 0.5])
-    mB = np.array([-1.3, -0.5])
+    mB = np.array([-1.5, -0.5])
     sigmaA = 0.5
     sigmaB = 0.5
 
     x = np.zeros([3, n_points*2])
     x[0,:n_points] = np.random.randn(1, n_points) * sigmaA + mA[0]
     x[1,:n_points] = np.random.randn(1, n_points) * sigmaA + mA[1]
-    x[2,:n_points] = 0
+    x[2,:n_points] = -1
     x[0,n_points:] = np.random.randn(1, n_points) * sigmaB + mB[0]
     x[1,n_points:] = np.random.randn(1, n_points) * sigmaB + mB[1]
     x[2,n_points:] = 1
@@ -37,7 +37,7 @@ def generate_binary_data():
 
 def plot_classes(X, Y):
     # force axis for "real-time" update in learning step
-    plt.axis([-3, 3, -3, 3])
+    #plt.axis([-3, 3, -3, 3])
     plt.scatter(X[0,:], X[1,:], c=Y[0,:])
     plt.show()
 
@@ -57,7 +57,7 @@ def draw_line(W, X):
 
 
 def generate_weights(X):
-    W = np.random.normal(0, 0.001, (1, X.shape[0]))
+    W = np.random.normal(0, 0.01, (1, X.shape[0]))
     return W
 
 
@@ -69,16 +69,42 @@ W = generate_weights(X)
 Perceptron Learning
 '''
 
-def Perceptron(X, Y, W, eta, n_epochs):
+def compute_cost(W, X, Y):
+    T = np.dot(W, X)
+    T = np.where(T > 0, 1, -1)
+    return np.sum((Y - T))
+
+
+
+def weigth_update(X, Y, W, T, eta, delta_rule=False):
+    if not delta_rule:
+        dW = eta * np.dot(T-Y, X.T)
+        #dW = -eta * np.dot(Y-T, X.T)
+    else:
+        e = T - np.dot(W, X)
+        dW = eta * np.dot(e, X.T)
+        #dW = -eta * np.dot((np.dot(W, X) - T), X.T)
+    return dW
+
+def perceptron(X, Y, W, eta, n_epochs, delta_rule=False, use_batch=True):
+
+    for i in range(0, n_epochs):
+        if use_batch:
+            T = np.dot(W, X)
+            T = np.where(T > 0, 1, -1)
+            W += weigth_update(X, Y, W, T, eta, delta_rule)
+        else:
+            for i in range(X.shape[1]):
+                x = X[:,i,None]
+                y = Y[:,i,None]
+                T = np.dot(W, x)
+                T = np.where(T > 0, 1, -1)
+                W += weigth_update(x, y, W, T, eta, delta_rule)
+
+        print("Error:", compute_cost(W, X, Y))
+
     draw_line(W, X)
     plot_classes(X, Y)
-    for i in range(0, n_epochs):
-        T = np.dot(W, X)
-        T = np.where(T > 0, 1, 0)
-        W -= eta * np.dot(T-Y, X.T)
-
-        draw_line(W, X)
-        plot_classes(X, Y)
 
 
-Perceptron(X, Y, W, 0.02, 200)
+perceptron(X, Y, W, 0.01, 5, delta_rule=False, use_batch=False)
