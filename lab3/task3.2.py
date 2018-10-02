42d84e69071bd2ab166a088bd51dee820f193cf9
victorhv <victorhv@kth.se>
2018-10-02 15:36:07 +0200

    Read data

--- lab3/task3.2.py
+++ lab3/task3.2.py
@@ -1,4 +1,7 @@
 import numpy as np
+
+
+pics = None
+
 with open('pict.dat') as f:
-    lines = np.array([int(p.strip()) for p in f.readlines()[0].split(',')])
-    print(lines.shape)
+    pics = np.fromfile(f, sep=',')
