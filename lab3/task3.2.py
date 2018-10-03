dda2bf9bd0a18905cbebd104c5dacb1eeba418af
erichall <hallstrom.eric@gmail.com>
2018-10-03 15:04:19 +0200

    energy plot

--- lab3/task3.2.py
+++ lab3/task3.2.py
@@ -52,37 +52,63 @@ def save_snapshot(X, iter, energy):
     plt.imshow(X.reshape(32,32))
     plt.savefig('./figures/' + str(iter) + '.png')
 
-def recall_sequential(X, W, steps):
+def recall_sequential(X, W, steps, random = False):
+    X, W = X.copy(), W.copy()
     E = 0
     counter = 0
-    save_snapshot(X, counter, E)
+    # save_snapshot(X, counter, E)
+    eng_saved = []
     for _ in range(steps):
         for p in range(X.shape[0]):
-            E = energy(X[0], W)
-            print('energy: ', E)
+            # E = energy(X[0], W)
+            # eng_saved.append(E)
+            # print('energy: ', E)
             for i in range(X.shape[1]):
                 a_i = 0
-                #idx = np.random.randint(X.shape[1])
-                idx = i
+                idx = np.random.randint(X.shape[1]) if random else i
                 for j in range(X.shape[1]):
                     if(idx == j):
                         continue
                     a_i += W[idx][j] * X[p][j]
-                x_new = np.sign(a_i)
-                x_old = X[p][idx]
-                s = a_i
+
+
                 X[p][idx] = np.sign(a_i)
                 counter += 1
-                if (counter % 100) == 0:
-                    save_snapshot(X, counter, energy(X[0], W))
+                # if (counter % 1) == 0:
+                E = energy(X[0], W)
+                eng_saved.append(E)
+                if(random):
+                    print('energy: ', E)
+                # pass
+                    # save_snapshot(X, counter, energy(X[0], W))
 
-    return X.astype(int)
+    return X.astype(int), eng_saved
 
 
+def task3_3_plot_energy:
+
+    W = little_model(pics[0:3,:])
+    steps = 10
+    ans_rand, eng_rand = recall_sequential(pics[9,None,:], W, steps=steps, random=True)
+    ans_seq, eng_seq = recall_sequential(pics[9,None,:], W, steps=steps, random=False)
+
+    x = np.arange(len(eng_rand))
+    
+    font = {'family' : 'normal',
+            'size'   : 18}
+    
+    plt.rc('font', **font)
+    plt.title('Sequential Recall Energy')
+    plt.plot(x, eng_rand, label='Random index')
+    plt.plot(x, eng_seq, label='Sequential index')
+    plt.legend()
+    plt.show()
 
 
-W = little_model(pics[0:3,:])
-ans = recall_sequential(pics[10,None,:], W, steps=10)
+# W = little_model(pics[0:3,:])
+# steps = 10
+# ans_rand, eng_rand = recall_sequential(pics[9,None,:], W, steps=steps, random=True)
+# ans_seq, eng_seq = recall_sequential(pics[9,None,:], W, steps=steps, random=False)
 
-show_pics(pics[0:3,:], 'Before 0-3')
-show_pics(ans, 'Recall: 0-3')
+# show_pics(pics, 'all')
+# show_pics(ans, 'Recall: 0-3')
