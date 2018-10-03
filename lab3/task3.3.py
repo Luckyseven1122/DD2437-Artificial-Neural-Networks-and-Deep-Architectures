4787369a4e417503388be4f731b26434299776f8
victorhv <victorhv@kth.se>
2018-10-03 14:41:54 +0200

    hmm

--- lab3/task3.3.py
+++ lab3/task3.3.py
@@ -2,19 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 
-def int_to_array(number):
-    '''
-    converts numbers in range 0 to 255 into lists
-    '''
-    assert number < 256 and number >= 0
-    res = []
-    for i in range(8):
-        res.append(1)
-        if (number & 128) == 0:
-            res[-1] = -1
-        number = number << 1
-    return np.array(res).reshape(1,-1)
-
 pics = None
 with open('pict.dat') as f:
     pics = np.fromfile(f, sep=',')
@@ -47,8 +34,6 @@ def little_model(X):
     P = X.shape[0] # P patterns
     N = X.shape[1] # N Units
     W = np.zeros((N,N))
-    W = np.random.normal(0, 2, W.shape)
-    #W = 0.5*(W+W.T)
     print('Number of patterns:', P)
     print('Number of units:', N)
     for p in range(P):
@@ -77,8 +62,8 @@ def recall_sequential(X, W, steps):
             print('energy: ', E)
             for i in range(X.shape[1]):
                 a_i = 0
-                idx = np.random.randint(X.shape[1])
-                #idx = i
+                #idx = np.random.randint(X.shape[1])
+                idx = i
                 for j in range(X.shape[1]):
                     if(idx == j):
                         continue
@@ -91,15 +76,13 @@ def recall_sequential(X, W, steps):
                 #if (counter % 100) == 0:
                     #save_snapshot(X, counter, energy(X[0], W))
 
-
     return X.astype(int)
 
 
 
 
-
 W = little_model(pics[0:3,:])
-ans = recall_sequential(pics[0,None,:], W, steps=10)
+ans = recall_sequential(pics[10,None,:], W, steps=10)
 
 show_pics(pics[0:3,:], 'Before 0-3')
 show_pics(ans, 'Recall: 0-3')
---
+++ lab3/task3.4.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = np.array([[-1,-1, 1,-1, 1,-1,-1, 1],
+                 [-1,-1,-1,-1,-1, 1,-1,-1],
+                 [-1, 1, 1,-1,-1, 1,-1, 1]])
+
+def little_model(data):
+
+    P = data.shape[0] # P patterns
+    N = data.shape[1] # N Units
+    W = np.zeros((N,N))
+    for i in range(P):
+        x = data[i,:].reshape(1,-1)
+        W += np.outer(x.T, x)
+    return W
+
+def recall(data, W):
+    P = data.shape[0]
+    for i in range(P):
+        x = np.sum(np.dot(W, data[i,:].reshape(1,-1).T), axis=1)
+        assert data[i,:].all() == np.where(x > 0, 1, -1).all()
+        print('data:  ',data[i,:])
+        print('recall:', np.where(x > 0, 1, -1))
+
+W = little_model(data)
+recall(data, W)
