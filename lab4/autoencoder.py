8112aecc376568e3343e3b7f21f4e197e9c795d2
victorhv <victorhv@kth.se>
2018-10-09 13:54:23 +0200

    Bäzda

--- lab4/autoencoder.py
+++ lab4/autoencoder.py
@@ -2,71 +2,79 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+import imp
 
 from plot import Plot
-from io_tools import get_training_data, get_testing_data
 
-X, _ = get_training_data()
-Y = X[0:2000]
-X = X[0:2000]
-plot = Plot()
 # -- plot examples --
 # plot.one(train[0])
 # plot.nine(train[0:9])
 # plot.custom(data = train[0:15],rows = 3, cols = 5)
 
-def autoencoder(inputs, hidden_size, reg_scale):
-    regularizer = tf.contrib.layers.l2_regularizer(scale=reg_scale)
+class network:
+    def __init__(self, X, Y, test_X, test_Y):
+        self.plot = Plot()
+        self.train_X = X
+        self.train_Y = Y
+        self.test_X = test_X
+        self.test_Y = test_Y
 
-    x = tf.contrib.layers.fully_connected(
-            inputs,
-            hidden_size,
-            weights_regularizer=regularizer,
-            activation_fn=tf.nn.sigmoid)
+    def autoencoder(self, inputs, hidden_size, reg_scale):
+        regularizer = tf.contrib.layers.l2_regularizer(scale=reg_scale)
 
-    decode = tf.contrib.layers.fully_connected(x, 784, activation_fn=None)
-    return decode
+        x = tf.contrib.layers.fully_connected(
+                inputs,
+                hidden_size,
+                weights_regularizer=regularizer,
+                activation_fn=tf.nn.sigmoid)
 
-
-def train(settings, batches, Y):
-    inputs = tf.placeholder(tf.float32, shape=[None, 784])
-    outputs = autoencoder(inputs = inputs,
-                          hidden_size = settings['hidden_size'],
-                          reg_scale= settings['reg_scale'])
-
-    loss = tf.reduce_mean(tf.square(inputs - outputs))
-    optimizer = tf.train.AdamOptimizer(learning_rate=settings['eta']).minimize(loss)
-
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-
-        for epoch in range(settings['epochs']):
-            for batch_idx, batch in enumerate(batches):
-                feed = { inputs: batch }
-                if(batch_idx % 30 == 0):
-                    _, cost = sess.run([optimizer, loss], feed_dict=feed)
-                optimizer.run(feed_dict=feed)
-
-            print('Epoch: ' + str(epoch) + ' Cost: ', cost)
-        Y = Y[0:settings['batch_size']]
-        reconstructed = sess.run(outputs, feed_dict={inputs: Y})
-
-        plot.custom(data = reconstructed, rows = 4, cols = 10)
-    return
-
-settings = {
-    'hidden_size': 500,
-    'num_batches': 50,
-    'epochs': 9,
-    'eta': 1e-3,
-    'reg_scale': 0.9
-}
+        decode = tf.contrib.layers.fully_connected(x, 784, activation_fn=None)
+        return decode
 
 
+    def train(self, settings, batches, Y):
+        inputs = tf.placeholder(tf.float32, shape=[None, 784])
+        outputs = self.autoencoder(inputs = inputs,
+                              hidden_size = settings['hidden_size'],
+                              reg_scale= settings['reg_scale'])
 
-settings['batch_size'] = int(X.shape[0] / settings['num_batches'])
-print('SETTINGS: ', json.dumps(settings, indent=2))
+        loss = tf.reduce_mean(tf.square(inputs - outputs))
+        optimizer = tf.train.AdamOptimizer(learning_rate=settings['eta']).minimize(loss)
 
-batches = np.array(np.array_split(X, settings['num_batches']))
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
 
-train(settings, batches, Y)
+            for epoch in range(settings['epochs']):
+                for batch_idx, batch in enumerate(batches):
+                    feed = { inputs: batch }
+                    if(batch_idx % 30 == 0):
+                        _, cost = sess.run([optimizer, loss], feed_dict=feed)
+                    optimizer.run(feed_dict=feed)
+
+                print('Epoch: ' + str(epoch) + ' Cost: ', cost)
+            Y = Y[0:settings['batch_size']]
+            reconstructed = sess.run(outputs, feed_dict={inputs: Y})
+
+            self.plot.custom(data = reconstructed, rows = 4, cols = 10)
+        return
+
+    def run(self):
+
+        X = self.train_X
+        Y = X[0:2000]
+        X = X[0:2000]
+
+        settings = {
+            'hidden_size': 500,
+            'num_batches': 50,
+            'epochs': 9,
+            'eta': 1e-3,
+            'reg_scale': 0.9
+        }
+
+        settings['batch_size'] = int(X.shape[0] / settings['num_batches'])
+        print('SETTINGS: ', json.dumps(settings, indent=2))
+
+        batches = np.array(np.array_split(X, settings['num_batches']))
+
+        self.train(settings, batches, Y)
---
+++ lab4/loader.py
@@ -0,0 +1,37 @@
+from io_tools import get_training_data, get_testing_data
+import autoencoder as _encoder
+from autoencoder import network
+import imp
+import sys
+
+class loader:
+    def pad(self, x, prompt):
+    	return "\n*- " + x.ljust(60, "-") + "._\n" + (">" if prompt else "")
+
+    def check_yes_no(self, x):
+    	while x.lower().strip() != "y" and x.lower().strip() != "n":
+    		x = input(self.pad("Run again?\nPlease make your selection: Y / N ", True))
+    	return True if x.lower().strip() == "y" else False
+
+    def __init__(self):
+        self.train_X, self.train_Y = get_training_data()
+        self.test_X, self.test_Y = get_testing_data()
+
+    def run(self):
+        while (True):
+            if self.check_yes_no(''):
+                try:
+                    imp.reload(_encoder)
+                    from autoencoder import network
+                    network(self.train_X.copy(),
+                            self.train_Y.copy(),
+                            self.test_X.copy(),
+                            self.test_Y.copy()).run()
+                except Exception as e:
+                    print('\n'+e)
+            else:
+                break
+
+if __name__ == '__main__':
+    l = loader()
+    l.run()
